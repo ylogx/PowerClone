786e1f0fc86b9982afde64d8bd42934b5e2d7449
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-05-13 12:48:04 +0530

    Use pandoc to convert md to rst

--- README.txt
+++ README.txt
@@ -1,13 +1,1 @@
-PowerClone [![Build Status](https://travis-ci.org/shubhamchaudhary/PowerClone.svg)](https://travis-ci.org/shubhamchaudhary/PowerClone) [![Coverage Status](https://coveralls.io/repos/shubhamchaudhary/PowerClone/badge.svg)](https://coveralls.io/r/shubhamchaudhary/PowerClone) [![Stories in Ready](https://badge.waffle.io/shubhamchaudhary/PowerClone.png?label=ready&title=Ready)](https://waffle.io/shubhamchaudhary/PowerClone)
-=========
-
-#### Installation
-PowerClone is available from PyPi
-
-```
-# pip install powerclone
-```
-
-
-#### Contributors
-Checkout CONTRIBUTION.md file
+README.md
--- setup.py
+++ setup.py
@@ -10,15 +10,19 @@ fhan = open('requirements.txt', 'rU')
 requires = [line.strip() for line in fhan.readlines()]
 fhan.close()
 #print('We require: ', requires)
-fhan = open('README.txt')
-long_description = fhan.read()
-fhan.close()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    fhan = open('README.txt')
+    long_description = fhan.read()
+    fhan.close()
 
 setup(
         name='PowerClone',
         description='PowerClone helps you clone all repos of an organisation on github',
-        version='0.0.7',
-        packages=['PowerClone'],
+        version='0.0.1',
+        packages=['powerclone'],
         license='GPLv3+',
         author='Shubham Chaudhary',
         author_email='me@shubhamchaudhary.in',
